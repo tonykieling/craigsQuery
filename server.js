dbffb76d7d0353da4f413295e27649d11b69b6a4
Tony Kieling <tony.kieling@gmail.com>
2019-08-05 10:19:53 -0700

    simple query returning to one variable all results for only one search point is working

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
 // https://www.npmjs.com/package/node-craigslist
+// dotenv
 
 const
   craigslist = require('node-craigslist'),
   client = new craigslist.Client({
     city : 'vancouver',
-  // }),
-  // options = {
-    // baseHost : "craigslist.ca", // defaults to craigslist.org
+  }),
+  options = {
+    name            : "Marine Drive",
     category        : "apa", // defaults to sss (all)
     postal          : "V5X0C7",
     searchDistance  : 1,
     minPrice        : '1000',
     maxPrice        : '1350'
-  });
- 
+  };
+
+myFunc = async () => {
+  console.log("inside myFunc");
+  const list = await client.list(options);
+  const details = await list.map(item => client.details(item));
+  const all = await Promise.all(details).then(v => v);
+  await console.log("+++++++++++++++++++++++++++++++++++ALL", all);
+}
+
+myFunc();
+
 // client
-//   .search(options)
-//   .then((listings) => {
-//     listings.forEach((listing) => console.log(listing));
-//   })
+//   .list(options)
+//   // .then(listings => listings.forEach((listing, i) => console.log(`${i + 1}### ${listing}`)))
+//   // .then(listings => console.log(listings.length))
+//   // .then(listings => listings.forEach((listing, i) => console.log(listings.length, i)))
+//   // .then(listings => listings.forEach(listing => client.details(listing, (err, result) => console.log("#####result", result))))
+//   .then(listings => listings.forEach(listing => client.details(listing, (err, result) => myArr.push(result))))
+//   .then(console.log(myArr))
 //   .catch((err) => {
 //     console.error(err);
 //   });
-client
-  .list()
-  // .then((listings) => listings.forEach(listing => console.log(listing)))
-  // .then((listings) => listings.forEach(listing => console.log(client.details(listing))))
-  .then((listings) => listings.forEach(listing => client.details(listing, (err, result) => console.log("#####result", result))))
-  // .then((details) => {
-  //   console.log(details);
-  // })
-  .catch((err) => {
-    console.error(err);
-  });
+
