820598c1d8d7aa4fa88abebd8064923da6a5ea4a
Tony Kieling <tony.kieling@gmail.com>
2019-08-05 17:44:01 -0700

    first version of simple content is done, need to call MailGun and send it by email)

--- server.js
+++ server.js
@@ -6,24 +6,95 @@ const
   client = new craigslist.Client({
     city : 'vancouver',
   }),
-  options = {
-    name            : "Marine Drive",
-    category        : "apa",
-    postal          : "V5X0C7",
-    searchDistance  : 1,
-    minPrice        : '1000',
-    maxPrice        : '1350'
-  };
+  options = [
+    {
+      name            : "MarineDrive",
+      category        : "apa",
+      postal          : "V5X0C7",
+      searchDistance  : 1,
+      minPrice        : '1000',
+      maxPrice        : '1350'
+    },
+    {
+      name            : "Langara",
+      category        : "apa",
+      postal          : "V5Y2Z9",
+      searchDistance  : 1,
+      minPrice        : '1000',
+      maxPrice        : '1350'
+    }    
+  ];
 
-myFunc = async () => {
-  console.log("inside myFunc");
-  const list = await client.list(options);
-  const details = await list.map(item => client.details(item));
-  const all = await Promise.all(details).then(v => v);
-  await console.log("+++++++++++++++++++++++++++++++++++ALL", all);
+// // query for only one option
+// myFunc = async () => {
+//   console.log("SIMPLE - inside myFunc");
+//   const list = await client.list(options);
+//   const details = await list.map(item => client.details(item));
+//   const all = await Promise.all(details).then(v => v);
+//   await console.log("+++++++++++++++++++++++++++++++++++ALL", all);
+// }
+
+// // query for multiple options
+// myFunc = async () => {
+//   console.log("MULTIPLE - inside myFunc");
+//   let lists = [];
+//   await options.forEach(async option => {
+//     lists[option.name] = await client.list(option);
+//     // await console.log(`=> ${option.name}`, lists[option.name]);
+//     // await console.log("-----------------------------------------------------------------");
+//   });
+
+//   setTimeout(() => {
+//     console.log("lists:::::::::::::::::::::::::::::::: \n", lists);
+//   }, 3000);
+//   // const lists = await options.map(option => {
+//   //   lists[option.name] = client.list(option);
+//   // });
+//   // await lists.forEach(item => console.log(item.name, item))
+//   // const details = await list.map(item => client.details(item));
+//   // const all = await Promise.all(details).then(v => v);
+//   // await console.log("+++++++++++++++++++++++++++++++++++ALL", all);
+// }
+
+// myFunc();
+formatPrint = (name, myArray) => {
+  const f = myArray.map((item, i) =>
+    `#item${i + 1} => PID: ${item.pid} - Title: ${item.title} - Price: ${item.price} - URL: ${item.url} \n`);
+
+  return(
+    `@Place Reference: ${name} \n` + f.join("")
+  );
 }
 
-myFunc();
+options.forEach(option => {
+  client
+    .list(option)
+    // .then(console.log)
+    // .then(answer => answer.map(item => client.details(item)))
+    // .then(result => Promise.all(result))
+    .then(final => console.log(formatPrint(option.name, final)))
+    // .list(option, (err, result) => {
+    //   if (err) throw err.message;
+    //   // console.log("result", result);
+    //   result.forEach((item, i) => {
+    //     // console.log(`${i} - ${option.name}\n ${JSON.stringify(item)}\n`);
+    //     console.log(`${i} - ${option.name}\n`);
+    //     client.details(item, async (err, resultDetails) => {
+    //       await console.log(`${i} => ${JSON.stringify(resultDetails)}`)
+    //     });
+    //   });
+    // });
+});
+  // // .then(listings => listings.forEach((listing, i) => console.log(`${i + 1}### ${listing}`)))
+  // // .then(listings => console.log(listings.length))
+  // // .then(listings => listings.forEach((listing, i) => console.log(listings.length, i)))
+  // // .then(listings => listings.forEach(listing => client.details(listing, (err, result) => console.log("#####result", result))))
+  // .then(listings => listings.forEach(listing => client.details(listing, (err, result) => myArr.push(result))))
+  // .then(console.log(myArr))
+  // .catch((err) => {
+  //   console.error(err);
+  // });
+
 
 // client
 //   .list(options)
