5d98ee83b744e9e30aa31ae5ecf385a5f0dd680a
Tony Kieling <tony.kieling@gmail.com>
2019-08-06 21:04:04 -0700

    first running - look at README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # craigsQuery
+It is sending an email for each place to research
+ - It has to be only one email for all researches
+It is not running undefinied
+  - It has to run without stop and send email every 30 minutes
+The messages need better format (HTML)
--- server.js
+++ server.js
@@ -65,58 +65,56 @@ const
 // myFunc();
 formatPrint = (name, myArray) => {
   const f = myArray.map((item, i) =>
-    `#item${i + 1} => PID: ${item.pid} - Title: ${item.title} - Price: ${item.price} - URL: ${item.url} \n`);
+    `#item${i + 1} => 
+      Title: ${item.title}
+      Price: ${item.price}
+      URL: ${item.url} \n\n`);
 
   return(
-    `@Place Reference: ${name} \n` + f.join("")
+    `@${name} \n` + f.join("")
   );
 }
 
-let msg = [];
-options.forEach(option => {
-  client
-    .list(option)
-    // .then(console.log)
-    // .then(answer => answer.map(item => client.details(item)))
-    // .then(result => Promise.all(result))
-    // .then(final => console.log(formatPrint(option.name, final)))
-    .then(result => {
-      msg.push(result);
-      const data = {
+myFunc = () => {
+  options.forEach(option => {
+    client
+      .list(option)
+      .then(answer => answer.map(item => client.details(item)))
+      .then(result => Promise.all(result))
+      .then(final => {
+        const data = {
           from: "Mailgun Sandbox <postmaster@sandbox002b4d3efa304a4a92fa6ba15da0460f.mailgun.org>",
           to: process.env.TO,
-          // cc: process.env.CC,
-          subject: "Hello",
-          text: formatPrint(option.name, msg)
+          cc: process.env.CC,
+          subject: option.name,
+          text: formatPrint(option.name, final)
         };
         
         mg.messages().send(data, function (error, body) {
             console.log("body", body);
         });
-    });
+      });
+  });
 
-
-    // .list(option, (err, result) => {
-    //   if (err) throw err.message;
-    //   // console.log("result", result);
-    //   result.forEach((item, i) => {
-    //     // console.log(`${i} - ${option.name}\n ${JSON.stringify(item)}\n`);
-    //     console.log(`${i} - ${option.name}\n`);
-    //     client.details(item, async (err, resultDetails) => {
-    //       await console.log(`${i} => ${JSON.stringify(resultDetails)}`)
-    //     });
-    //   });
-    // });
-});
-  // // .then(listings => listings.forEach((listing, i) => console.log(`${i + 1}### ${listing}`)))
-  // // .then(listings => console.log(listings.length))
-  // // .then(listings => listings.forEach((listing, i) => console.log(listings.length, i)))
-  // // .then(listings => listings.forEach(listing => client.details(listing, (err, result) => console.log("#####result", result))))
-  // .then(listings => listings.forEach(listing => client.details(listing, (err, result) => myArr.push(result))))
-  // .then(console.log(myArr))
-  // .catch((err) => {
-  //   console.error(err);
+  // const data = {
+  //   from: "Mailgun Sandbox <postmaster@sandbox002b4d3efa304a4a92fa6ba15da0460f.mailgun.org>",
+  //   to: process.env.TO,
+  //   // cc: process.env.CC,
+  //   subject: "Hello",
+  //   text: formatPrint(option.name, msg)
+  // };
+  
+  // mg.messages().send(data, function (error, body) {
+  //     console.log("body", body);
   // });
+}
+
+// while(1) {
+//   setTimeout(() => {
+      myFunc();
+//     }, 15000
+//   );
+// }
 
 
 // client
