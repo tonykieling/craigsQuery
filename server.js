57b9b9f65a2e9488f864318eb72ad3cca921ea93
Tony Kieling <tony.kieling@gmail.com>
2019-08-11 09:58:53 -0700

    code cleaned and working, sending email with correct data

--- server.js
+++ server.js
@@ -40,122 +40,45 @@ const
     }
   ];
 
-// // query for only one option
-// myFunc = async () => {
-//   console.log("SIMPLE - inside myFunc");
-//   const list = await client.list(options);
-//   const details = await list.map(item => client.details(item));
-//   const all = await Promise.all(details).then(v => v);
-//   await console.log("+++++++++++++++++++++++++++++++++++ALL", all);
-// }
-
-// // query for multiple options
-// myFunc = async () => {
-//   console.log("MULTIPLE - inside myFunc");
-//   let lists = [];
-//   await options.forEach(async option => {
-//     lists[option.name] = await client.list(option);
-//     // await console.log(`=> ${option.name}`, lists[option.name]);
-//     // await console.log("-----------------------------------------------------------------");
-//   });
-
-//   setTimeout(() => {
-//     console.log("lists:::::::::::::::::::::::::::::::: \n", lists);
-//   }, 3000);
-//   // const lists = await options.map(option => {
-//   //   lists[option.name] = client.list(option);
-//   // });
-//   // await lists.forEach(item => console.log(item.name, item))
-//   // const details = await list.map(item => client.details(item));
-//   // const all = await Promise.all(details).then(v => v);
-//   // await console.log("+++++++++++++++++++++++++++++++++++ALL", all);
-// }
-
-// myFunc();
 formatPrint = (myArray) => {
-  // console.log("myArray", myArray.name);
-  // myArray.forEach(item => console.log("item", item.name));
   const f = myArray.map((item, i) =>
-    // console.log("item", item);
-    // if (item.url) {
       `#item${i + 1} => 
         Title: ${item.title}
         Price: ${item.price}
         URL: ${item.url} \n\n`
-    // }
   );
-  // console.log("f---", f);
   return(`@${myArray.name} - total of ${Object.keys(myArray).length - 1} \n` + f.join(""));
 }
 
-myFunc = async () => {
-  let arr = [];
-  const x = await options.map(async item => {
-    const z = await client.list(item);
-    z["name"] = item.name;
-    // console.log("item", z);
-    return z;
-    // await console.log("z", z);
-  });
-  const y = await Promise.all(x);
-  // await console.log("---------------------", y);
-  const final = await y.map(item => {
-    // console.log("item", item);
-    return formatPrint(item);
-  });
-  const kk = await Promise.all(final);
-  // await console.log("kk", kk);
-// const final = y.map(item => {
-//   const k = await(item.name, item);
-// });
 
-  // const abc = await y.map(async item => {
-  // //   await item.forEach(async newItem => {
-  // //     console.log("url", newItem.url);
-  // //   });    
-  //   const result = await item.map(async newItem => {
-  //     const zed = await client.details(newItem);
-  //     return zed;
-  //   });
-  //   const final = await Promise.all(result);
-  //   await console.log("final", final);
-  //   // return final;
-  // });
-  // const f = await Promise.all(abc);
-  // await console.log("****************", f);
-// }
-// myFunc = () => {
-//   options.forEach(option => {
-//     client
-//       .list(option)
-//       .then(answer => answer.map(item => client.details(item)))
-//       .then(result => Promise.all(result))
-//       .then(final => {
-//         const data = {
-//           from: "Mailgun Sandbox <postmaster@sandbox002b4d3efa304a4a92fa6ba15da0460f.mailgun.org>",
-//           to: process.env.TO,
-//           cc: process.env.CC,
-//           subject: option.name,
-//           text: formatPrint(option.name, final)
-//         };
-        
-//         mg.messages().send(data, function (error, body) {
-//             console.log("body", body);
-//         });
-//       });
-//   });
-
+sendEmail = (content) => {
   const data = {
     from: "Mailgun Sandbox <postmaster@sandbox002b4d3efa304a4a92fa6ba15da0460f.mailgun.org>",
     to: process.env.TO,
-    cc: process.env.CC,
+    // cc: process.env.CC,
     subject: dateFormat(new Date(), "dddd  -  mmmm dS, yyyy  -  HH:MM"),
-    text: kk
+    text: content
   };
   
   mg.messages().send(data, function (error, body) {
       console.log("body", body);
+  });  
+}
+
+myFunc = async () => {
+  const getList = await options.map(async item => {
+    const eachItem = await client.list(item);
+    eachItem["name"] = item.name;
+    return eachItem;
   });
+  const list = await Promise.all(getList);
+  const getFormated = await list.map(item => {
+    return formatPrint(item);
+  });
+  const data = await Promise.all(getFormated);
+
+  sendEmail(data);
+
 }
 
 // while(1) {
@@ -164,17 +87,4 @@ myFunc = async () => {
 //     }, 15000
 //   );
 // }
-
-
-// client
-//   .list(options)
-//   // .then(listings => listings.forEach((listing, i) => console.log(`${i + 1}### ${listing}`)))
-//   // .then(listings => console.log(listings.length))
-//   // .then(listings => listings.forEach((listing, i) => console.log(listings.length, i)))
-//   // .then(listings => listings.forEach(listing => client.details(listing, (err, result) => console.log("#####result", result))))
-//   .then(listings => listings.forEach(listing => client.details(listing, (err, result) => myArr.push(result))))
-//   .then(console.log(myArr))
-//   .catch((err) => {
-//     console.error(err);
-//   });
 
