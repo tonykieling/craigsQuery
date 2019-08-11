f13b4586c1b75aafc606671d6263b33ce4ae7f57
Tony Kieling <tony.kieling@gmail.com>
2019-08-11 16:07:21 -0700

    doing html format using ejs

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ const
     city : 'vancouver',
   }),
   options = [
-    {
-      name            : "Langara",
-      category        : "apa",
-      postal          : "V5Y2Z9",
-      searchDistance  : 1,
-      minPrice        : '1000',
-      maxPrice        : '1350'
-    },
+    // {
+    //   name            : "Langara",
+    //   category        : "apa",
+    //   postal          : "V5Y2Z9",
+    //   searchDistance  : 1,
+    //   minPrice        : '1000',
+    //   maxPrice        : '1350'
+    // },
     {
       name            : "Okridge",
       category        : "apa",
@@ -53,11 +53,12 @@ formatPrint = (myArray) => {
 
 sendEmail = (content) => {
   const data = {
-    from: "Mailgun Sandbox <postmaster@sandbox002b4d3efa304a4a92fa6ba15da0460f.mailgun.org>",
-    to: process.env.TO,
+    from    : "Mailgun Sandbox <postmaster@sandbox002b4d3efa304a4a92fa6ba15da0460f.mailgun.org>",
+    to      : process.env.TO,
     // cc: process.env.CC,
-    subject: dateFormat(new Date(), "dddd  -  mmmm dS, yyyy  -  HH:MM"),
-    text: content
+    subject : dateFormat(new Date(), "dddd  -  mmmm dS, yyyy  -  HH:MM"),
+    html    : content
+    // text    : content
   };
   
   mg.messages().send(data, function (error, body) {
@@ -66,18 +67,45 @@ sendEmail = (content) => {
 }
 
 myFunc = async () => {
+  let ejs = require('ejs');
+  // ejs.renderFile("./index.ejs", {urls: {shortURL: "short", longtURL: "long"}}, options, function(err, str){
+  //   if (err)
+  //     console.log("### err", err);
+  //   else
+  //     sendEmail(str);
+  // });
+
   const getList = await options.map(async item => {
     const eachItem = await client.list(item);
     eachItem["name"] = item.name;
     return eachItem;
   });
   const list = await Promise.all(getList);
-  const getFormated = await list.map(item => {
-    return formatPrint(item);
+  // console.log("list", list);
+  // for(const item of list) {
+  //   // console.log("item.name", item);
+  //   for(const x in item) {
+  //     console.log("x==>", x);
+  //     console.log("x==>", item[x]);
+  //   }
+  // }
+  // await list.forEach(item => console.log("itemList", item.name));
+  
+  await ejs.renderFile("./formatHTML.ejs", {list}, options, function(err, result){
+    if (err)
+      console.log("### err", err);
+    else
+      // console.log("+++result", result)
+      sendEmail(result);
   });
-  const data = await Promise.all(getFormated);
 
-  sendEmail(data);
+
+  // const getFormated = await list.map(item => {
+  //   return formatPrint(item);
+  // });
+  // const data = await Promise.all(getFormated);
+  // console.log("***data", data);
+  // sendEmail(data);
 
 }
 
