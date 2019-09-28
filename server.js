0ff0e96a3095e3172187f3ba537d32346fea6ecc
Tony Kieling <tony.kieling@gmail.com>
2019-09-28 11:52:20 -0700

    new file structure is done and working

--- server.js
+++ server.js
@@ -1,28 +1,22 @@
-// https://www.npmjs.com/package/node-craigslist
-// dotenv
-// mailgun
+const { hasChange }           = require("./aux/hasChange.js");
+const { formatDataToBeSent }  = require("./aux/formatDataToBeSent.js");
 
 const dateFormat  = require('dateformat');
-const ejs           = require('ejs');
-const mailgun     = require("mailgun-js");
 
-require("dotenv").config();
-
-const mg          = mailgun({apiKey: process.env.APIKEY, domain: process.env.DOMAIN});
-
-const gMinPrice   = 1100,
-      gMaxPrice   = 1350;
+const gMinPrice     = 1100,
+      gMaxPrice     = 1350
+      defaultRatio  = 0.7;
 
 let beforeData    = null;
 
 const
-  craigslist  = require('node-craigslist'),
-  client      = new craigslist.Client({
+  craigslist      = require('node-craigslist'),
+  client          = new craigslist.Client({
     city : 'vancouver',
   }),
-  generalOptions = {
+  generalOptions  = {   // they are shared for all objects that will be queried
       category        : "apa",
-      searchDistance  : 0.7,
+      searchDistance  : defaultRatio,
       minPrice        : gMinPrice,
       maxPrice        : gMaxPrice
   }
@@ -71,88 +65,6 @@ const
 
 
 
-// this function only checks whether the data before and current are the same
-// if so, the system may not send the email because nothing has changed
-hasChange = (before, current) => {
-  let someChange = false;
-  for (let c of current)
-    for (let b of before)
-      if (b.name === c.name) {
-        for (let objC of c) {
-          let countC = 0;
-          for (let objB of b) {
-            if (objB.pid === objC.pid) {
-              objB.flag = true;
-              if (objB.price !== objC.price) {
-                objC.modify     = "Changed";
-                someChange      = true;
-              }
-              break;
-            } else {
-              countC += 1;
-              if (countC === b.length) {
-                someChange      = true;
-                objC.modify     = "New";
-                break;
-              }
-            }
-          }
-        }
-        for (let objB of b)
-          if (!objB.flag) {
-            objB.modify  = "Deleted";
-            someChange   = true;
-            c.push(objB);
-          }
-      }
-  return(someChange && current);
-}
-
-
-
-// function to send email using MailGun
-sendEmail = (content, subject) => {
-  const data = {
-    from    : "Mailgun Sandbox <postmaster@sandbox002b4d3efa304a4a92fa6ba15da0460f.mailgun.org>",
-    to      : process.env.TO,
-    cc      : process.env.CC,
-    subject,
-    html    : content
-    // text    : content
-  };
-  
-  mg.messages().send(data, function (error, body) {
-    console.log("### sending email ==> ", body.message);
-  });  
-}
-
-
-
-// it formats the data to be sent by the email function
-// it is called just when need to send email, right before that
-formatDataToBeSent = (list, flag) => {
-  ejs.renderFile("./formatHTML.ejs", {list, gMinPrice, gMaxPrice}, options, function(err, result){
-    if (err)
-      console.log("### err", err);
-    else {
-      const d = new Date(),
-            utc = d.getTime() + (d.getTimezoneOffset() * 60000),
-            nd = new Date(utc + (3600000*-7));
-
-      let subject = "";
-
-      if (flag === "first")
-        subject = `fromZero - ${dateFormat(nd, "@HH:MM - dddd  -  dd/mm/yyyy")}`;
-      else
-        subject = `${dateFormat(nd, "@HH:MM - dddd  -  dd/mm/yyyy")}`;
-
-      sendEmail(result, subject);
-    }
-  });
-}
-
-
-
 // main function of the system
 // this function that queries craigslist
 // it calls an auxiliary function to check if there is change between the last query and the current one
@@ -170,7 +82,7 @@ mainFunc = async () => {
     // first time the system runs it send the email with the received data from craigslist
     console.log("FIRST TIME");
     const flag = "first";
-    formatDataToBeSent(list, flag);
+    formatDataToBeSent(list, flag, gMinPrice, gMaxPrice);
 
   } else {
     // it calls a function to compare list and beforeData, if there are changes
@@ -201,28 +113,10 @@ mainFunc = async () => {
 mainController = () => {
   clearInterval(secondT);
   mainFunc();   
-  // const timeDay   = 1000 * 60 * 15
-  //       timeNight = 1000 * 60 * 30;
-  let interval    = 1000 * 60 * 15;
-  // the code above will run just once
-  
+  let interval    = 1000 * 60 * 15;  
 
   // this function runs forever, while this application is running
   setInterval(() => {
-    // const d = new Date(),
-    //       utc = d.getTime() + (d.getTimezoneOffset() * 60000),
-    //       currentTime = new Date(utc + (3600000 * -7));
-
-    // according to the current time, the variable interval is set
-    // at night, btw 22 - 6 it will run every 30 minutes
-    // at day, it runs every 15 minutes
-    //////////////NOT WORKING NEED TO KILL THE SETINTERVAL
-    // if (Number(dateFormat(currentTime, "HH")) > 6 &&
-    //     (Number(dateFormat(currentTime, "HH")) < 22))
-    //   interval = timeDay;
-    // else
-    //   interval = timeNight;
-
     mainFunc();
   }, interval);
 }
@@ -235,11 +129,14 @@ mainController = () => {
 fFifteen = () => {
   clearInterval(firstT);
   secondT = setInterval(() => {
-    const cTime = Number(dateFormat(new Date(), "MM"));
-    console.log(`#15sec running @${cTime}`);
+    const d = new Date(),
+          utc = d.getTime() + (d.getTimezoneOffset() * 60000),
+          cTime = new Date(utc + (3600000 * -7));
 
-    if ((cTime % 15) === 0) {
-      console.log("got FIFTEEN", dateFormat(new Date(), "HH:MM:ss"));
+    console.log(`#15sec running @${dateFormat(cTime, "HH:MM:ss")}`);
+
+    if (((dateFormat(cTime, "MM")) % 15) === 0) {
+      console.log("got FIFTEEN", dateFormat(cTime, "HH:MM:ss"));
       mainController();
     }
 
@@ -253,11 +150,15 @@ fFifteen = () => {
 // it's gonna be executed only once
 fZero = () => {
   firstT = setInterval(() => {
-    const t = Number(dateFormat(new Date(), "ss"));
+    const d = new Date(),
+          utc = d.getTime() + (d.getTimezoneOffset() * 60000),
+          cTime = new Date(utc + (3600000 * -7));
+
+    const t = dateFormat(cTime, "ss");
     console.log("time = ", t);
 
-    if (t === 0 || t === 1) {
-      console.log("got ZERO", dateFormat(new Date(), "HH:MM:ss"));
+    if (t === "00" || t === "01") {
+      console.log("got ZERO", dateFormat(cTime, "HH:MM:ss"));
       fFifteen();
 
     }
@@ -267,7 +168,10 @@ fZero = () => {
 
 // it runs at the very beggining to send the first email
 // it runs only once
-console.log(`# first running @${dateFormat(new Date(), "HH:MM:ss")}`);
+const d = new Date(),
+      utc = d.getTime() + (d.getTimezoneOffset() * 60000),
+      cTime = new Date(utc + (3600000 * -7));
+console.log(`# first running @${dateFormat(cTime, "HH:MM:ss")}`);
 mainFunc();
 let firstT  = null;
 let secondT = null;
